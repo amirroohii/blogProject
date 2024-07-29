8698917b796a9aa40c911fd918b7c65726f32ec1
IMRUUHI\roohi <roohiamir931@gmail.com>
2024-07-29 17:41:15 +0330

    validation for sign up serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import User
+from rest_framework.validators import ValidationError
 
 class SignUpSerializer(serializers.ModelSerializer):
     email = serializers.CharField(max_length=50)
@@ -9,3 +10,10 @@ class SignUpSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'username', 'password']
+
+    def validate(self, attrs):
+        email_exists = User.objects.filter(email__iexact=attrs['email']).exists()
+        if email_exists:
+            return ValidationError('you have already an account')
+
+        return super().validate(attrs)
